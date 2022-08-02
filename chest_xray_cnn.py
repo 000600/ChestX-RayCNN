c91115e008cf34998b0779527405f06d4d5f0b0e
Aiden Gandhi <90153180+000600@users.noreply.github.com>
2022-08-02 14:51:50 -0600

    Update chest_xray_cnn.py

--- chest_xray_cnn.py
+++ chest_xray_cnn.py
@@ -59,6 +59,7 @@ model.add(Flatten())
 model.add(Dense(128, activation = 'relu'))
 model.add(Dropout(0.2))
 model.add(Dense(64, activation = 'relu'))
+model.add(Dense(32, activation = 'relu'))
 
 # Output layer
 model.add(Dense(4, activation = 'softmax'))  # Softmax activation function since the model is multiclass
